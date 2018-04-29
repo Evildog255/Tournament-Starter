7598fc38bf219e7190bf0e3b25a94fdebeeddd4f
Evildog255 <superninja8@live.nl>
2018-04-30 00:24:13 +0200

    Add files via upload

---
+++ Tournament starter.py
@@ -0,0 +1,30 @@
+import discord
+#import sqlite3
+from bs4 import BeautifulSoup
+import html
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as:\n\t%s\n\t%s\n\n" % (client.user.name, client.user.id))
+    await client.change_presence(game=discord.Game(name=status))
+
+@client.event
+async def on_member_join(member):   
+    await client.send_message(member.server, "Welcome %s to the server" % member.name)
+@client.event
+async def on_member_remove(member):
+    await client.send_message(member.server, "%s has left or has been kicked!" % member.name)
+@client.event
+async def on_message(message):
+	nowTime = datetime.now().strftime("%H:%M:%S")
+	print("%s\n\t%s\t\n\t%s" % (nowTime,message.author,message.clean_content))
+	command = message.content.split(" ")[0]
+	if command == ("!tournament"):
+    	await client.send_message(message.channel,"tournament work in progress!")
+
+client.run('')
